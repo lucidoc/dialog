67b9be5409a390299740dfe727513bbf112bbf4e
Danny Paez <danny.paez@lucidoc.com>
2024-01-22 15:26:10 -0800

    using osascript for dialogs on Mac

--- dlgs_darwin.go
+++ dlgs_darwin.go
@@ -1,19 +1,53 @@
 package dialog
 
 import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+
 	"github.com/lucidoc/dialog/cocoa"
 )
 
 func (b *MsgBuilder) yesNo() bool {
-	return cocoa.YesNoDlg(b.Msg, b.Dlg.Title)
+	expression := fmt.Sprintf("tell app \"System Events\" to display dialog \"%s\" buttons {\"No\", \"Yes\"} default button 2 with title \"%s\"", b.Msg, b.Dlg.Title)
+	cmd := exec.Command("osascript", "-e", expression)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	cmd.Start()
+
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	} else {
+		output := strings.Trim(out.String(), "\n\r ")
+		output = strings.ToLower(output)
+		regex := regexp.MustCompile("(no|yes)")
+		output = regex.FindString(output)
+		return output == "yes"
+	}
 }
 
 func (b *MsgBuilder) info() {
-	cocoa.InfoDlg(b.Msg, b.Dlg.Title)
+	expression := fmt.Sprintf("tell app \"System Events\" to display dialog \"%s\" buttons {\"Ok\"} default button 1 with title \"%s\"", b.Msg, b.Dlg.Title)
+	cmd := exec.Command("osascript", "-e", expression)
+	cmd.Start()
+
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
 }
 
 func (b *MsgBuilder) error() {
-	cocoa.ErrorDlg(b.Msg, b.Dlg.Title)
+	expression := fmt.Sprintf("tell app \"System Events\" to display dialog \"Error:\n\n%s\" buttons {\"Ok\"} default button 1 with title \"%s\"", b.Msg, b.Dlg.Title)
+	cmd := exec.Command("osascript", "-e", expression)
+	cmd.Start()
+
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
 }
 
 func (b *FileBuilder) load() (string, error) {
